1475e0aa7aca9c5d9ca26fa0781fd7635c71b320
jhcode33 <jhcode33@gmail.com>
2023-09-14 11:07:28 +0900

    Feat: Change ch07-model4 Main, JOINED, SINGE_TABLE
    - 상속관계 매핑에 대한 내용을 추가하고, Inheritance 속성을 변경하며 쿼리를 확인했습니다

--- ch07-model4/src/main/java/jpabook/model/Main.java
+++ ch07-model4/src/main/java/jpabook/model/Main.java
@@ -1,5 +1,6 @@
 package jpabook.model;
 
+import jpabook.model.entity.Member;
 import jpabook.model.entity.item.Album;
 import jpabook.model.entity.item.Book;
 import jpabook.model.entity.item.Movie;
@@ -8,6 +9,8 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import java.time.LocalDateTime;
+import java.util.Date;
 
 /**
  * Created by 1001218 on 15. 4. 5..
@@ -26,7 +29,7 @@ public class Main {
 
             tx.begin(); //트랜잭션 시작
             //TODO 비즈니스 로직
-            savaBook(em);
+            saveBook(em);
             tx.commit();//트랜잭션 커밋
 
         } catch (Exception e) {
@@ -39,7 +42,7 @@ public class Main {
         emf.close(); //엔티티 매니저 팩토리 종료
     }
 
-    public static void savaBook(EntityManager em) {
+    public static void saveBook(EntityManager em) {
         Book book = new Book();
         book.setAuthor("jh");
         book.setIsbn("1234");
@@ -54,5 +57,19 @@ public class Main {
         movie.setActor("unknown");
         movie.setDirector("sad");
         em.persist(movie);
+
+        em.flush();
+        em.clear();
+
+        Book findBook = em.find(Book.class, book.getId());
+        System.out.println("Book Author: " + findBook.getAuthor());
+        System.out.println("Book ISBN: " + findBook.getIsbn());
+    }
+
+    public static void saveOrderAndMember(EntityManager em) {
+        Member member = new Member();
+        member.setName("jh");
+        member.setCity("Dague");
+        member.setStreet("sagin");
     }
 }
