71982fd384975bef5c1d80ded288c361ef75c1a5
jhcode33 <jhcode33@gmail.com>
2023-09-18 11:21:31 +0900

    Feat: Add useEmbedded
    - Embedded Projection 사용하고 결과를 확인했습니다

--- ch09-ex/src/main/java/jpabook/model/Main.java
+++ ch09-ex/src/main/java/jpabook/model/Main.java
@@ -1,9 +1,6 @@
 package jpabook.model;
 
-import jpabook.model.entity.Address;
-import jpabook.model.entity.Member;
-import jpabook.model.entity.Order;
-import jpabook.model.entity.Team;
+import jpabook.model.entity.*;
 
 import javax.persistence.*;
 import java.util.Date;
@@ -24,7 +21,7 @@ public class Main {
         try {
 
             tx.begin(); //트랜잭션 시작
-            saveStub(em);
+            Long[] ids = saveStub(em);
             tx.commit();//트랜잭션 커밋
             em.clear();
 
@@ -40,6 +37,9 @@ public class Main {
             //== use Entity Projection ==//
             useEntityProjection(em);
 
+            //== use Embedded ==//
+            useEmbedded(em);
+
         } catch (Exception e) {
             e.printStackTrace();
             tx.rollback(); //트랜잭션 롤백
@@ -49,7 +49,7 @@ public class Main {
 
         emf.close(); //엔티티 매니저 팩토리 종료
     }
-    public static void saveStub(EntityManager em) {
+    public static Long[] saveStub(EntityManager em) {
         System.out.println("============================== save stub ==========================");
         Team team = new Team();
         team.setName("team");
@@ -58,13 +58,32 @@ public class Main {
         Member member1 = new Member();
         member1.setName("member1");
         member1.setTeam(team);
+        em.persist(member1);
 
         Member member2 = new Member();
         member2.setName("member2");
         member2.setTeam(team);
-
-        em.persist(member1);
         em.persist(member2);
+
+        Product productA = new Product();
+        productA.setName("productA");
+        em.persist(productA);
+
+        Address address1 = new Address();
+        address1.setCity("Dague");
+        address1.setStreet("Sangin");
+        address1.setZipcode("41281");
+        //em.persist(address1); // Entity가 아니므로 Embedded를  사용하는 쪽에서 저장해야함.
+
+        Order order = new Order();
+        order.setMember(member1);
+        order.setProduct(productA);
+        order.setAddress(address1);
+        em.persist(order);
+
+        Long[] ids = new Long[10];
+        ids[0] = order.getId();
+        return ids;
     }
 
     public static void useTypeQuery(EntityManager em) {
@@ -122,17 +141,22 @@ public class Main {
         TypedQuery<Team> queryTeam =
                 em.createQuery("SELECT m.team From Member m", Team.class);
 
+        // m.team이 무엇인지 이해하기
         List<Team> resultTeamList = queryTeam.getResultList();
         for (Team team : resultTeamList) {
-            System.out.println("id: " + team.getId());
             System.out.println("name: " + team.getName());
+            for (Member member : team.getMembers()) {
+                System.out.println("name: " + member.getName());
+            }
         }
     }
 
 
-//    public static void useEmbedded(EntityManager em) {
-//        System.out.println("=============================== use Embedded ============================");
-//        Order order = new Order();
-//        order.set
-//    }
+    public static void useEmbedded(EntityManager em) {
+        em.clear();
+        System.out.println("=============================== use Embedded ============================");
+
+        List<Address> addresses =
+                em.createQuery("SELECT o.address FROM Order o", Address.class).getResultList();
+    }
 }
